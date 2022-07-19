55545e24dc3cd82835983eb33034b2b83f0f8fe2
instructor <webinstructor19@gmail.com>
2022-07-19 19:23:20 +0200

    initial commit

---
+++ QuizSolution.py
@@ -0,0 +1,109 @@
+# No.1
+def palindrome(number):
+    print("original number", number)
+    original_num = number
+    
+    # reverse the given number
+    reverse_num = 0
+    while number > 0:
+        reminder = number % 10
+        reverse_num = (reverse_num * 10) + reminder
+        number = number // 10
+
+    # check numbers
+    if original_num == reverse_num:
+        print("Given number palindrome")
+    else:
+        print("Given number is not palindrome")
+
+palindrome(121)
+palindrome(125)
+
+# No.2
+def merge_list(list1, list2):
+    result_list = []
+    
+    # iterate first list
+    for num in list1:
+        # check if current number is odd
+        if num % 2 != 0:
+            # add odd number to result list
+            result_list.append(num)
+    
+    # iterate second list
+    for num in list2:
+        # check if current number is even
+        if num % 2 == 0:
+            # add even number to result list
+            result_list.append(num)
+    return result_list
+
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+print("result list:", merge_list(list1, list2))
+
+# No.3
+def exponent(base, exp):
+    num = exp
+    result = 1
+    while num > 0:
+        result = result * base
+        num = num - 1
+    print(base, "raises to the power of", exp, "is: ", result)
+
+exponent(5, 4)
+
+# No.4
+n = 2
+# stop: 11 (because range never include stop number in result)
+# run loop 10 times
+for i in range(1, 11, 1):
+    # 2 *i (current number)
+    product = n * i
+    print(product)
+
+#No.5
+# Solution1
+list1 = [10, 20, 30, 40, 50]
+# reverse list
+new_list = reversed(list1)
+# iterate reversed list
+for item in new_list:
+    print(item)
+# Solution2 Using Loop
+list1 = [10, 20, 30, 40, 50]
+# get list size
+# len(list1) -1: because index start with 0
+# iterate list in reverse order
+# star from last item to first
+size = len(list1) - 1
+for i in range(size, -1, -1):
+    print(list1[i])
+
+# No.6
+start = 25
+end = 50
+print("Prime numbers between", start, "and", end, "are:")
+
+for num in range(start, end + 1):
+    # all prime numbers are greater than 1
+    # if number is less than or equal to 1, it is not prime
+    if num > 1:
+        for i in range(2, num):
+            # check for factors
+            if (num % i) == 0:
+                # not a prime number so break inner loop and
+                # look for next number
+                break
+        else:
+            print(num)
+
+# No.7
+# Fizz Buzz Game
+
+# No.8
+def check(t):
+    return all(i == t[0] for i in t)
+
+tuple1 = (45, 45, 45, 45)
+print(check(tuple1))
